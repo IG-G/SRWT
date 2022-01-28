a7bf90c8d8c326ddc99b50acaf38a0445f7b08a0
IG-G <igolos@op.pl>
2022-01-28 18:05:13 +0100

    Create django app

--- API/main.py
+++ API/main.py
@@ -3,6 +3,7 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 import logging
+import mysql.connector
 
 
 class Item(BaseModel):
@@ -11,6 +12,15 @@ class Item(BaseModel):
     price: float
     tax: Optional[float] = None
 
+
+db_connection = mysql.connector.connect(
+    host="localhost",
+    user="api",
+    password="passwordAPI",
+    database="testreport"
+)
+
+cursor = db_connection.cursor()
 
 app = FastAPI()
 
@@ -25,4 +35,9 @@ async def create_item(item: Item):
     logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
     logging.warning('This will get logged to a')
     logging.warning('Test ' + item.name)
+
+    sql = "INSERT INTO TestCampaign(id, name, status) VALUES (2, 'WEB2', 'END')"
+    cursor.execute(sql)
+
+    db_connection.commit()
     return "Ok"
