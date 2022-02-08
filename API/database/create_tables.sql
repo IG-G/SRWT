3df910034c237214074443677acde2b69419fcff
IG-G <igolos@op.pl>
2022-02-08 23:33:50 +0100

    Added EndTestCampaign endpoint

--- API/database/create_tables.sql
+++ API/database/create_tables.sql
@@ -1,8 +1,10 @@
 use testreport;
 drop table TestCampaign;
 create table TestCampaign(
-id INT(6),
-repositoryName VARCHAR(30),
-envName VARCHAR(30),
-begTime TIMESTAMP
+    id INT(6),
+    repositoryName VARCHAR(30),
+    envName VARCHAR(30),
+    begTime DATETIME,
+    endTime DATETIME DEFAULT null,
+    status VARCHAR(30)
 );
--- API/main.py
+++ API/main.py
@@ -1,15 +1,17 @@
-from typing import Optional
-
-from fastapi import FastAPI
-from pydantic import BaseModel
-import logging
 import mysql.connector
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 
-class TestCampaignParams(BaseModel):
+class TestCampaignInitParams(BaseModel):
     repositoryName: str
     envName: str
     dateOfBeginning: str
+
+
+class TestCampaignEndingParams(BaseModel):
+    status: str
+    dateOfEnding: str
 
 
 db_connection = mysql.connector.connect(
@@ -18,44 +20,32 @@ db_connection = mysql.connector.connect(
     password="passwordAPI",
     database="testreport"
 )
-
 cursor = db_connection.cursor()
-
 test_campaigns_id = [0]
-
 app = FastAPI()
 
 
-@app.get("/")
-async def get_index():
-    return "Hello word"
-
-
 @app.post("/campaigns/")
-async def create_campaign(campaign: TestCampaignParams):
-
+async def create_campaign(campaign: TestCampaignInitParams):
     i = test_campaigns_id[-1]
     i = i + 1
     test_campaigns_id.append(i)
 
-    sql = "INSERT INTO TestCampaign(id, repositoryName, envName, begTime) VALUES (" \
-          + i.__str__() + ", " + campaign.repositoryName + ", " + campaign.envName + ", " + campaign.dateOfBeginning + \
-          ")"
+    sql = "INSERT INTO TestCampaign(id, repositoryName, envName, begTime, status) VALUES ('" \
+          + i.__str__() + "', '" + campaign.repositoryName + "', '" + campaign.envName + "', '" \
+          + campaign.dateOfBeginning + "', '" + "STARTED" + "')"
     cursor.execute(sql)
-
     db_connection.commit()
-
     return i
 
 
-@app.post("/items/")
-async def create_item(item: TestCampaignParams):
-    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-    logging.warning('This will get logged to a')
-    logging.warning('Test ' + item.repositoryName)
-
-    sql = "INSERT INTO TestCampaign(id, name, status) VALUES (2, 'WEB2', 'END')"
+@app.put("/campaigns/{campaign_id}")
+async def end_campaign(
+        campaign: TestCampaignEndingParams,
+        campaign_id: int = Path(..., title="Campaign ID from POST request")
+):
+    sql = "UPDATE TestCampaign SET endTime = '" + campaign.dateOfEnding + "', status = '" + campaign.status \
+          + "' WHERE id = " + campaign_id.__str__() + ";"
     cursor.execute(sql)
-
     db_connection.commit()
-    return "Ok"
+    return
